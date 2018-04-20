a16c5fa670f280f7e7a73da689b79ac23d4df027
Priscilla <samiduh@live.com>
2018-04-20 13:39:14 +0100

    Added menu-page for customer

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,6 +1,34 @@
-var btn = document.querySelector('#js-btn');
+// var btn = document.getElementById('#js-btn');
 
-btn.addEventListener('mouseover', function() {
+// btn.addEventListener('mouseover', function () {
 
-    btn.classList.add('btn');
-});
+//      btn.classList.add('btn');
+//  });
+
+//Display date
+var dateHolder = document.querySelector('#js-date'); 
+
+//Function to format date
+const formatDate = (date) => {
+
+    var months = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+    
+    var days = [
+        "Sunday","Monday", "Tuesday","Wednesday",
+         "Thursday", "Friday","Saturday",          
+    ];
+
+    var dayIndex = date.getDay();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+  
+    return days[dayIndex] + ' ' + months[monthIndex] + ', ' + year;
+  };
+
+var today = new Date();
+dateHolder.innerHTML = formatDate(today);
