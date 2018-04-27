aaf6c5abeb2a54810bf23751b1c83e6c5ef63660
PriscillaSam <samiduh@live.com>
2018-04-27 15:03:48 +0100

    Add api endpoint to update a meal [Finishes #157145189]

--- server/src/controllers/meal.js
+++ server/src/controllers/meal.js
@@ -1,13 +1,52 @@
 import meals from '../models/meal';
 
-
+/**
+ * @class MealController
+ */
 class Meal {
 
     static getMeals(req,res) {
-        return res.json({
+        return res.status(200).json({
             meals
         });
-    }    
+    } 
+    
+/**
+ * @method updateMeal
+ * @param {object} req
+ * @param {object} res 
+ */
+    static updateMeal(req , res) {
+        //get mealid
+        const mealId = parseInt(req.params.mealId);
+        //Check that it is available
+        const meal = meals.find(meal => meal.mealId == mealId);
+        const mealIndex = meals.findIndex(meal => meal.mealId == meal.mealId); 
+        if(!meal) {
+            return res.status(404).send({
+                success: 'false',
+                message: 'this meal does not exist'
+            });
+        }
+        
+        //update values
+        const updatedMeal = {            
+            mealId: meal.mealId,
+            name: req.body.name || meal.name,
+            description: req.body.description || meal.description,
+            price : parseInt(req.body.price) || meal.price
+
+        }
+
+        meals.splice(mealIndex, 1 , updatedMeal);
+        return res.status(200).send({
+            updatedMeal,
+            success: 'true',
+            message: 'meal successfully updated',
+
+        });
+
+    }
 
 }
 
--- server/tests/meal.test.js
+++ server/tests/meal.test.js
@@ -1,20 +1,68 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import {expect} from 'chai';
+import app from '../app';
 
 chai.use(chaiHttp);
 
-
  describe('Meal Controller ', () => {
     
-    it('should return a status code of 200 if meals are sucessfully gotten.', () => {
+    it('should return a status code of 200 if meals are sucessfully gotten.', (done) => {
         chai.request('http://localhost:3000')
         .get('/api/v1/meals')
         .end((err, res) => {
+            expect(res.body).to.be.a('array');
             expect(res).to.have.status(200);
             done();
         });
     });
     
+ });
+
+ describe('Update a meal api route', () => {
+
+    const mealUpdate = {
+        name: 'Chicken',
+        description: 'We love chicken'
+    };
+
+    const updatedMeal = {
+        mealId:1,
+        name:'Chicken',
+        description: 'we love chicken',
+        price: 1500
+    };
+
+     it('should update an existing meal in the database', (done) => {
+        chai.request(app)
+            .put('/api/v1/meals/1')
+            .send({
+                mealUpdate
+            })
+            .end(res => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.equal({
+                    updatedMeal,
+                    success: 'true',
+                    message: 'meal successfully updated',
+                });
+                done();
+            });
+     });
+
+     it('should return an error 404 if the meal is not found', (done) => {
+        chai.request(app)
+            .put('/api/v1/meals/5')
+            .send(mealUpdate)
+            .end((err, res) => {
+                expect(res).to.have.status(404);
+                expect(res.body).to.equal({
+                    success: 'false',
+                    message: 'this meal does not exist'
+                });
+                done();
+
+            });
+     });
  });
 
