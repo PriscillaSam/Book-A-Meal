199ef0b52afa658bf6b55f3cfcb89e1453246a15
PriscillaSam <samiduh@live.com>
2018-04-27 15:51:00 +0100

    Add api endpoint to remove a meal

--- server/src/controllers/meal.js
+++ server/src/controllers/meal.js
@@ -9,6 +9,28 @@ class Meal {
         });
     }    
 
+
+    static removeMeal(req, res) {
+        //find meal 
+        const mealId = parseInt(req.params.mealId);        
+
+        const meal = meals.find(meal => meal.mealId == mealId);
+        const mealIndex = meals.findIndex(meal => meal.mealId == mealId);
+
+        if(!meal) {
+            return res.status(404).send({
+                success:'false',
+                message: 'this meal does not exist'
+            });
+        }
+        //else delete meal
+        meals.splice(mealIndex , 1);
+        return res.status(200).send({
+            success:'true',
+            message:'meal successfully removed'
+        });
+
+    }
 }
 
 export default Meal;
