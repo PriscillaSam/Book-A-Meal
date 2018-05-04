dbb08ff2068bfa1c9a127d11ccc94fc6acf97813
PriscillaSam <samiduh@live.com>
2018-05-04 15:26:01 +0100

    Fix order confirmation modal

--- UI/js/modals.js
+++ UI/js/modals.js
@@ -1,24 +1,30 @@
 const modal = document.getElementById('order-modal');
 const orderBtn = document.querySelector('.order-btn');
 const closeBtn = document.getElementsByClassName('closeBtn')[0];
+const confirmBtn = document.getElementById('confirmBtn');
+const confirmModal = document.getElementById('confirm');
+const close  = document.getElementById('close');
 
+orderBtn.addEventListener('click', (e) =>  {
 
-orderBtn.addEventListener('click', () =>  {
-
-    modal.classList.remove('hidden');
-    //modal.style.display = 'block';
+    e.preventDefault();
+    modal.style.display = 'block';
     
 });
 
-closeBtn.onclick = () => {
+closeBtn.onclick = (e) => {
+    
+     modal.style.display = 'none';
+};
+
+close.addEventListener('click', () => {
+    confirmModal.style.display = 'none';
+});
+
+confirmBtn.addEventListener('click', (e) => {
+    e.preventDefault();
     modal.style.display = 'none';
-}
-
-window.addEventListener('click', (e) => {
-
-    if(e.target !== modal) {
-        modal.style.display = 'none';
-    }
+    confirmModal.style.display = 'block';
 });
 
 function deleteBtn(item) {
@@ -26,9 +32,8 @@ function deleteBtn(item) {
 
         if(confirm('Are you sure you want to remove this meal option')) {
             alert('This meal has been deleted');
-        }
-    
+        }   
 
     });
-}
+};
 
